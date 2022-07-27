b6b714faf8c66c416e44a9eb2ffafdfcbd99467f
Valentino <collazo.valentino@gmail.com>
2022-07-27 11:40:57 -0300

    First Commit

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,29 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsApiHeaders = {
+  "X-BingApis-SDK": "true",
+  "X-RapidAPI-Key": "bc1e97e144msh5300c285a92e484p1e09e9jsn4597dc843138",
+  "X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com",
+};
+
+const baseUrl = "https://bing-news-search1.p.rapidapi.com";
+
+const createRequest = (url) => ({
+  url,
+  headers: cryptoNewsApiHeaders,
+});
+
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+        ),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
